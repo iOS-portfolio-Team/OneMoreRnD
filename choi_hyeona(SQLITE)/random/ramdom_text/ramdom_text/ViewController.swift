1be0433a71b102d45174c15db1b90290f7c17c9a
chlgusdk28 <hyeona_c@naver.com>
2021-02-22 14:29:14 +0900

    no message

--- choi_hyeona(SQLITE)/random/ramdom_text/ramdom_text/ViewController.swift
+++ choi_hyeona(SQLITE)/random/ramdom_text/ramdom_text/ViewController.swift
@@ -11,7 +11,18 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let randomText = ["회원님 오늘은 하체하셔야죠 :D",
+                          "식단 잘 지키고 계시죠 ? 전 회원님을 믿습니다. :D",
+                          "흔들리면 그것은 지방입니다 :D",
+                          "헬스클럽은 클럽보다 더 즐거운곳 :D",
+                          "오늘도 화이팅 ! 아자아자 우리회원님 할수있다! :D",
+                          "회원님~ 한번만 더 하실수있죠? :D",
+                          "우리회원님 ! 내일도 나오실거죠? 도망가면 안돼요 :D",
+                          "할수있다 우리 회원님! 한번만 더! :D"]
+        
+        let random = Int(arc4random_uniform(7)) //1 or 0
+        print(randomText[random])
+
     }
 
 
